223a1cdf1e34aadb85aa56278b5fbd1d46289c09
gagaprince <wangzidongprince@163.com.com>
2021-02-19 09:07:24 +0800

    feat:调优

--- src/DownLoad.ts
+++ src/DownLoad.ts
@@ -3,156 +3,171 @@ import axios from 'axios';
 import DownloadMoreThread from './DownloadMoreThread';
 
 export interface DownloadOptions {
-    url: string;
-    filePath: string;
-    retry?: number;
-    timeout?:number;
-    onProgress?: (progress: number) => void;
-    onSuccess?: () => void;
-    onFailed?: (error: string) => void;
+  url: string;
+  filePath: string;
+  retry?: number;
+  timeout?: number;
+  threadCount?: number;
+  type?: number;
+  onProgress?: (progress: number) => void;
+  onSuccess?: () => void;
+  onFailed?: (error: string) => void;
 }
 
 export default class Download {
-    private retry: number = 3;
-    private url: string;
-    private filePath: string;
-    private timeout: number;
-    private onFailed: (error: string) => void = (error: string) => { console.log(error); };
-    private onProgress: ((progress: number) => void) | undefined;
-    private onSuccess: (() => void) | undefined;
-    public constructor(opts: DownloadOptions) {
-        const { url, filePath, retry,timeout=5000, onProgress, onSuccess, onFailed } = opts;
-        this.url = url;
-        this.filePath = filePath;
-        this.retry = retry || this.retry;
-        this.timeout = timeout;
-        this.onFailed = onFailed || this.onFailed;
-        this.onProgress = onProgress;
-        this.onSuccess = onSuccess;
-    }
+  private retry: number = 3;
+  private url: string;
+  private filePath: string;
+  private timeout: number;
+  private threadCount: number;
+  private onFailed: (error: string) => void = (error: string) => {
+    console.log(error);
+  };
+  private onProgress: ((progress: number) => void) | undefined;
+  private onSuccess: (() => void) | undefined;
+  public constructor(opts: DownloadOptions) {
+    const {
+      url,
+      filePath,
+      retry,
+      timeout = 5000,
+      onProgress,
+      onSuccess,
+      onFailed,
+      threadCount = 10,
+    } = opts;
+    this.url = url;
+    this.filePath = filePath;
+    this.retry = retry || this.retry;
+    this.timeout = timeout;
+    this.onFailed = onFailed || this.onFailed;
+    this.onProgress = onProgress;
+    this.onSuccess = onSuccess;
+    this.threadCount = threadCount;
+  }
 
-    public async start() {
+  public async start() {
+    try {
+      const response = await axios({
+        url: this.url,
+        method: 'GET',
+        responseType: 'stream',
+        timeout: this.timeout, // responsType是stream 连接5s未响应就算超时
+      });
+      const inputStream = response.data;
+
+      const length = response.headers['content-length'] || 1;
+      console.log(`${this.url} 文件长度:${this.getFileSize(length)}`);
+      inputStream.destroy();
+      if (length < 1024 * 1024 * 10) {
+        //文件小于10M 单线程下载
+        console.log('文件小于10M 直接单线程下载');
+        await this.downloadOneThread();
+      } else {
+        // 大于10M 多线程下载
+        console.log('文件大于10M 启用多线程下载');
+        // inputStream.destroy();//先关闭当前流 开启多线程下载
+        await new DownloadMoreThread({
+          downloadUrl: this.url,
+          desFile: this.filePath,
+          threadCount: this.threadCount,
+          length,
+          onProgress: this.onProgress,
+        }).start();
+      }
+    } catch (e) {
+      setTimeout(() => {
+        this.onFailed(e.toString());
+      });
+      return;
+    }
+    setTimeout(() => {
+      this.onSuccess && this.onSuccess();
+    });
+  }
+
+  private async downloadOneThread() {
+    this.mkFile(this.filePath);
+    const writer = fs.createWriteStream(this.filePath);
+    const response = await axios({
+      url: this.url,
+      method: 'GET',
+      responseType: 'stream',
+      timeout: this.timeout,
+    });
+    const inputStream = response.data;
+
+    inputStream.pipe(writer);
+    const length = response.headers['content-length'] || 1;
+    let hasDownloadLength = 0;
+
+    const doProgress = (newLength: number) => {
+      hasDownloadLength += newLength;
+      const progress = hasDownloadLength / length;
+      this.onProgress && this.onProgress(progress);
+    };
+
+    const onError = async (resolve: any, reject: any) => {
+      inputStream.destroy();
+      writer.destroy();
+      if (this.retry > 0) {
+        this.retry--;
+        fs.removeSync(this.filePath);
         try {
-            const response = await axios({
-                url: this.url,
-                method: 'GET',
-                responseType: 'stream',
-                timeout: this.timeout,// responsType是stream 连接5s未响应就算超时
-            });
-            const inputStream = response.data;
-
-            const length = response.headers['content-length'] || 1;
-            console.log(`${this.url} 文件长度:${this.getFileSize(length)}`);
-            inputStream.destroy();
-            if (length < 1024 * 1024 * 10) {//文件小于10M 单线程下载
-                console.log('文件小于10M 直接单线程下载');
-                await this.downloadOneThread();
-            } else { // 大于10M 多线程下载
-                console.log('文件大于10M 启用多线程下载');
-                // inputStream.destroy();//先关闭当前流 开启多线程下载
-                await new DownloadMoreThread({
-                    downloadUrl: this.url,
-                    desFile: this.filePath,
-                    threadCount: 10,
-                    length,
-                    onProgress: this.onProgress
-                }).start();
-            }
+          await this.downloadOneThread();
+          resolve('');
         } catch (e) {
-            setTimeout(()=>{
-                this.onFailed(e.toString());
-            });
-            return;
+          reject(e);
         }
-        setTimeout(()=>{
-            this.onSuccess && this.onSuccess();
-        });
-    }
+      } else {
+        reject(`下载失败,重试${this.retry}次`);
+      }
+    };
 
-    private async downloadOneThread() {
-        this.mkFile(this.filePath);
-        const writer = fs.createWriteStream(this.filePath);
-        const response = await axios({
-            url: this.url,
-            method: 'GET',
-            responseType: 'stream',
-            timeout: this.timeout,
-        });
-        const inputStream = response.data;
-
-        inputStream.pipe(writer);
-        const length = response.headers['content-length'] || 1;
-        let hasDownloadLength = 0;
-
-        const doProgress = (newLength: number) => {
-            hasDownloadLength += newLength;
-            const progress = hasDownloadLength / length;
-            this.onProgress && this.onProgress(progress);
+    return new Promise((resolve, reject) => {
+      let timeoutHandle: any = setTimeout(() => {
+        console.log('超过10s没有新的数据产生，下载超时');
+        onError(resolve, reject);
+      }, this.timeout);
+      inputStream.on('data', (chunk: any) => {
+        if (timeoutHandle) {
+          clearTimeout(timeoutHandle);
         }
-
-        const onError = async (resolve: any, reject: any) => {
-            inputStream.destroy();
-            writer.destroy();
-            if (this.retry > 0) {
-                this.retry--;
-                fs.removeSync(this.filePath);
-                try {
-                    await this.downloadOneThread();
-                    resolve('');
-                } catch (e) {
-                    reject(e);
-                }
-            } else {
-                reject(`下载失败,重试${this.retry}次`);
-            }
+        timeoutHandle = setTimeout(() => {
+          console.log('超过10s没有新的数据产生，下载超时');
+          onError(resolve, reject);
+        }, this.timeout);
+        doProgress(chunk.length);
+      });
+      writer.on('finish', (data: any) => {
+        if (timeoutHandle) {
+          clearTimeout(timeoutHandle);
         }
+        resolve('');
+      });
+      writer.on('error', (e: any) => {
+        onError(resolve, reject);
+      });
+    });
+  }
 
-        return new Promise((resolve, reject) => {
-            let timeoutHandle: any = setTimeout(() => {
-                console.log('超过10s没有新的数据产生，下载超时');
-                onError(resolve, reject);
-            }, this.timeout);
-            inputStream.on('data', (chunk: any) => {
-                if (timeoutHandle) {
-                    clearTimeout(timeoutHandle);
-                }
-                timeoutHandle = setTimeout(() => {
-                    console.log('超过10s没有新的数据产生，下载超时');
-                    onError(resolve, reject);
-                }, this.timeout);
-                doProgress(chunk.length);
-            });
-            writer.on('finish', (data: any) => {
-                if (timeoutHandle) {
-                    clearTimeout(timeoutHandle);
-                }
-                resolve('');
-            });
-            writer.on('error', (e: any) => {
-                onError(resolve, reject);
-            });
-        });
+  private getFileSize(length: number): string {
+    const ksize = length / 1024;
+    if (ksize < 1024) {
+      return ksize.toFixed(2) + 'k';
     }
-
-    private getFileSize(length: number): string {
-        const ksize = length / 1024;
-        if (ksize < 1024) {
-            return ksize.toFixed(2) + 'k';
-        }
-        const msize = ksize / 1024;
-        if (msize < 1024) {
-            return msize.toFixed(2) + 'M';
-        }
-        const gsize = msize / 1024;
-        return gsize.toFixed(2) + 'G';
+    const msize = ksize / 1024;
+    if (msize < 1024) {
+      return msize.toFixed(2) + 'M';
     }
+    const gsize = msize / 1024;
+    return gsize.toFixed(2) + 'G';
+  }
 
-    private mkFile(filePath: string) {
-        const path = filePath.substr(0, filePath.lastIndexOf('/') + 1);
-        fs.ensureDirSync(path);
-    }
+  private mkFile(filePath: string) {
+    const path = filePath.substr(0, filePath.lastIndexOf('/') + 1);
+    fs.ensureDirSync(path);
+  }
 
-    private downloadThreads() {
-
-    }
+  private downloadThreads() {}
 }
--- test.ts
+++ test.ts
@@ -1,19 +1,49 @@
 import { download } from './src/index';
 const path = require('path');
 
-const mypath = path.resolve(__dirname, 'tmp/a.mp4');
-console.log(mypath);
-
-const url = 'http://vjs.zencdn.net/v/oceans.mp4';
-
-download({
-    url, filePath: mypath, onFailed: (error: string) => {
+async function downloadFile(url: string, title: string) {
+  const mypath = path.resolve(__dirname, `tmp/${title}.mp4`);
+  return new Promise((res, rej) => {
+    download({
+      url,
+      filePath: mypath,
+      threadCount: 20,
+      onFailed: (error: string) => {
         console.log(`失败原因！！！:${error}`);
-    },
-    onSuccess: () => {
+        res('');
+      },
+      onSuccess: () => {
         console.log('文件下载成功！');
-    },
-    onProgress: (progress) => {
+        res('');
+      },
+      onProgress: (progress) => {
         console.log(`进度:${Math.floor(progress * 100)}%`);
-    }
-});
+      },
+    });
+  });
+}
+
+async function main() {
+  await downloadFile(
+    'https://dd02.520cc.cc/ddll/mp4/7/7b7lU.mp4?sk=AoMcTJHzVojWf_8_q5jRvQ&se=1613573000',
+    'HBAD-405 嬌軀白嫩的未亡人 就在丈夫的遺像前被內射 紗紗原百合[中文字幕]'
+  );
+  //   await downloadFile(
+  //     'https://vsb02.520call.me/files/mp4/O/O5SAa.mp4?sk=3u1sAOj_H0B0amVpz7PWGQ&se=1613505771',
+  //     'VOSS-096 『不要！現在動了的話……就停不下來了！』繼母憐憫童貞又總是自慰的我，同意和我股間摩擦！為了讓肉棒蹭到陰蒂而扭腰的時候肉棒一下插'
+  //   );
+  //   await downloadFile(
+  //     'https://vsb02.520call.me/files/mp4/Y/YqqEI.mp4?sk=QNEpZ6NgkRA6aKKS9JpGjg&se=1613506081',
+  //     'DOCP-180 抓住家事派遣員的弱點來追擊抽插強制中出！ 2[有碼高清中文字幕]'
+  //   );
+  //   await downloadFile(
+  //     'https://vsb01.520call.me/files/mp4/Y/YbBbU.mp4?sk=ggqgIVtMDMoUkzXlAxcfAg&se=1613505104',
+  //     'HUNTA-518 巨乳妹妹玩起素股還開插！雙腿夾腰逼我中出！[中文字幕]'
+  //   );
+  //   await downloadFile(
+  //     'https://vsb02.520call.me/files/mp4/f/fXKk2.mp4?sk=7nZnz0XjkXHwrI3X_RqmeQ&se=1613505150',
+  //     'HUNTA-529 因為太興奮了，巨乳家教幫我素股卻受不了插了進去內射了！[中文字幕]'
+  //   );
+}
+
+main();
